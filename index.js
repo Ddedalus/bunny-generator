e907d5e7ddbd430cda89da55529b4fafefc8a56a
Hubert Bereś <h.beres@hotmail.com>
2019-08-01 00:13:30 +0100

    Brute-force 3s wait for the redirect request

--- index.js
+++ index.js
@@ -3,33 +3,38 @@
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-    // Your code here
-    app.log('Yay, the app was loaded!')
-  
-    app.on(['issues.opened', 'issues.closed', 'issues.reopened'], (context) => {
-      context.log("Processing an issue...")    
+  // Your code here
+  app.log('Yay, the app was loaded!')
 
-      const query = 'bunny';
-      const url = `https://source.unsplash.com/random/?${query}`;
-      // const context = context;
-      var https = require('https');
-      https.get(url, (res) => {
-        context.log('Redirect:', res.headers.location);
-        
-        const bunny_link = res.headers.location || url;
-        const body = `Good job! Enjoy:\n\
+  app.on(['issues.opened', 'issues.closed', 'issues.reopened'], async (context) => {
+    context.log("Processing an issue...")
+
+    const query = 'bunny';
+    const url = `https://source.unsplash.com/random/?${query}`;
+    // const context = context;
+    var https = require('https');
+    https.get(url, (res) => {
+      context.log('Redirect:', res.headers.location);
+
+      const bunny_link = res.headers.location || url;
+      const body = `Good job! Enjoy:\n\
         ![Cute bunny](${bunny_link})`
-        context.log("Publishing an issue:", body)
-        
-        const issueComment = context.issue({ body: body})
-        context.github.issues.createComment(issueComment)
-  
+      context.log("Publishing an issue:", body)
+
+      const issueComment = context.issue({ body: body })
+      context.github.issues.createComment(issueComment)
+
+    });
+    function wait() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => resolve("hello"), 3000)
       });
+    }
+    await wait();
     })
-    // For more information on building apps:
-    // https://probot.github.io/docs/
-  
-    // To get your app running against GitHub, see:
-    // https://probot.github.io/docs/development/
-  }
-  
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
