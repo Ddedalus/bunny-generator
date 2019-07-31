f2e66312d9e06b1f539ac275c597a6ea929dc118
Hubert Bereś <h.beres@hotmail.com>
2019-07-31 12:15:54 +0100

    Pasting a random image from unsplash.com
    https.get is used to grab a redirect link appearing in their /random/?query.
    This may have some trouble with synchronisation as I don't know how the callbacks exactly work.
    I removed async keyword from the github.issues.createComment() function but it appears to still work...

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { get } from 'https'
 
 export = (app: Application) => {
-  app.on(['issues.opened', 'issues.closed'], async (context) => {
+  app.on(['issues.opened', 'issues.closed', 'issues.reopened'], async (context) => {
+    
+    get('https://source.unsplash.com/random/?bunny', (res) => {
+      console.log('Redirect:', res.headers.location);
+      
+      const bunny_link: string = res.headers.location;
+      const body = `Good job! Enjoy:\n\
+      ![Cute bunny](${bunny_link})`
+      console.log("Publishing an issue:", body)
+      
+      const issueComment = context.issue({ body: body})
+      context.github.issues.createComment(issueComment)
 
-    const body = 'Good job! Enjoy:\n\
-    ![Cute bunny](https://www.bing.com/th?id=OIP.EGnMzdVyePJWuvk6dZOJqgHaEo&pid=Api&rs=1)'
-    const issueComment = context.issue({ body: body})
-    await context.github.issues.createComment(issueComment)
+    }).on('error', (e) => {
+      console.error(e);
+    });
   })
   // For more information on building apps:
   // https://probot.github.io/docs/
