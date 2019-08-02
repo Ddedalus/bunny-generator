96eda610712037f345004bcea32b7388d37ba6e0
Hubert Bereś <h.beres@hotmail.com>
2019-08-02 09:54:06 +0100

    Conditional messages and cleanup

--- index.js
+++ index.js
@@ -7,24 +7,30 @@ module.exports = app => {
   app.log('Yay, the app was loaded!')
 
   app.on(['pull_request.opened', 'pull_request.closed', 'pull_request.reopened'], async (context) => {
-    context.log(context)
-    context.log("Processing an issue...")
+    context.log("Processing a pull request...")
     context.log("Was merged?:", context.payload.pull_request.merged)
+
     const query = 'bunny';
     const url = `https://source.unsplash.com/random/?${query}`;
-    // const context = context;
-    var https = require('https');
-    https.get(url, (res) => {
-      context.log('Redirect:', res.headers.location);
 
-      const bunny_link = res.headers.location || url;
-      const body = `Good job! Enjoy:\n\
-        ![Cute bunny](${bunny_link})`
+    require('https').get(url, (res) => {
+      // this is slightly hacky as they are generating a redirection link
+      const bunny_link = res.headers.location;
+      context.log('Redirect target:', bunny_link);
+
+      if (context.payload.pull_request.merged & context.payload.action == 'closed') {
+        const praise = 'Good job on closing that one! Enjoy your reward:';
+      } else if (context.payload.action == 'opened') {
+        const praise = 'A new pull request, great! Look here...';
+      } else {
+        const praise = `Well, dunno what\'s goinng on. Grab a ${query} anyway:`;
+      }
+
+      const body = `${praise}\n![${query}](${bunny_link})`
       const issueComment = context.issue({ body: body })
 
-      context.log("Publishing an issue:", issueComment)
+      context.log("Commenting:", issueComment)
       context.github.issues.createComment(issueComment)
-
     });
     function wait() {
       return new Promise((resolve, reject) => {
